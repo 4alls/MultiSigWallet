81ee989f75ae3c4a55f66c6c99e6b6332b40bc48
ThibautBaudry <thibaut.baudry@gmail.com>
2025-05-15 15:30:29 +0200

    Wallet Creation

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.30;
+
+contract MultiSigWallet {
+
+    uint public quorum;
+
+    mapping(address account => bool isSigner) public signers;
+
+    constructor(uint _quorum, address[] memory _signers) {
+        quorum = _quorum;
+
+        for (uint i = 0; i < _signers.length; i++) {
+            address signer = _signers[i];
+            signers[signer] = true;
+        }
+    }
+}
