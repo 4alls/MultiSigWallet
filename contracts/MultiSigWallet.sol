56ed87e0c806a260cf3da5e0152874bed4ad795d
ThibautBaudry <thibaut.baudry@gmail.com>
2025-05-15 15:41:27 +0200

    Validate Transaction

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -14,6 +14,7 @@ contract MultiSigWallet {
     Transaction[] public transactions;
 
     mapping(address account => bool isSigner) public signers;
+    mapping(uint id => mapping(address signer => bool validated)) public validations;
 
     constructor(uint _quorum, address[] memory _signers) {
         quorum = _quorum;
@@ -27,5 +28,13 @@ contract MultiSigWallet {
     function proposeTransaction(address _to, uint _value) external {
         require(signers[msg.sender] == true, "Not signer");
         transactions.push(Transaction({to: _to, value: _value, votes: 1}));
+    }
+
+    function validateTransaction(uint _id) external {
+        require(signers[msg.sender] == true, "Not signer");
+        require(validations[_id][msg.sender] == false, "Already validated");
+
+        transactions[_id].votes ++;
+        validations[_id][msg.sender] = true;
     }
 }
