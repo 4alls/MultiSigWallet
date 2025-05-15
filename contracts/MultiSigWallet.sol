29df007977a6ad58b3273030bfe8d44282736d09
ThibautBaudry <thibaut.baudry@gmail.com>
2025-05-15 16:30:14 +0200

    Validate and execute transaction

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -8,33 +8,60 @@ contract MultiSigWallet {
         address to;
         uint value;
         uint votes;
+        uint deadline;
+        bool executed;
     }
 
     uint public quorum;
     Transaction[] public transactions;
-
+    
     mapping(address account => bool isSigner) public signers;
     mapping(uint id => mapping(address signer => bool validated)) public validations;
 
+    modifier onlySigner() {
+        require(signers[msg.sender] == true, "Not signer");
+        _;
+    }
+
     constructor(uint _quorum, address[] memory _signers) {
+        require(_quorum > 1 && _quorum <= _signers.length);
         quorum = _quorum;
 
         for (uint i = 0; i < _signers.length; i++) {
             address signer = _signers[i];
+            require(signer != address(0), "Address zero");
+            require(signers[signer] == false, "Duplicate signer");
+
             signers[signer] = true;
         }
     }
 
-    function proposeTransaction(address _to, uint _value) external {
-        require(signers[msg.sender] == true, "Not signer");
-        transactions.push(Transaction({to: _to, value: _value, votes: 1}));
+    function transactionCount() external view returns (uint) {
+        return transactions.length;
     }
 
-    function validateTransaction(uint _id) external {
-        require(signers[msg.sender] == true, "Not signer");
+    function proposeTransaction(address _to, uint _value) external onlySigner {
+        uint id = transactions.length;
+        transactions.push(Transaction({to: _to, value: _value, votes: 1, deadline: block.timestamp + 1 days, executed: false}));
+        validations[id][msg.sender] = true;
+    }
+
+    function validateTransaction(uint _id) external onlySigner {
         require(validations[_id][msg.sender] == false, "Already validated");
+        require(transactions[_id].executed == false, "Already executed");
+        require(block.timestamp < transactions[_id].deadline, "Invalid transaction");
 
         transactions[_id].votes ++;
         validations[_id][msg.sender] = true;
+
+        if (transactions[_id].votes >= quorum) {
+            address payable to = payable(transactions[_id].to);
+            uint value = transactions[_id].value;
+            to.transfer(value);
+        }
     }
+
+    receive() external payable {}
 }
+
+
